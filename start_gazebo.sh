529f95802cdffb3cd644f85ac606dd8791ff5ec0
Wil <wfkoch@gmail.com>
2018-05-15 14:54:16 -0400

    fix paths

--- start_gazebo.sh
+++ start_gazebo.sh
@@ -1,10 +1,10 @@
 
 GZ_VERSION=$(gazebo --version | grep -Po "(?<=version )(\d)" | head -1)
 
-source /usr/share/gazebo-$(GZ_VERSION)/setup.sh
+source /usr/share/gazebo-${GZ_VERSION}/setup.sh
 gazebo_assets=../aeroloop_gazebo
 export GAZEBO_MODEL_PATH=${gazebo_assets}/models:${GAZEBO_MODEL_PATH}
 export GAZEBO_RESOURCE_PATH=${gazebo_assets}/resources:${GAZEBO_RESOURCE_PATH}
 export GAZEBO_PLUGIN_PATH=${gazebo_assets}/plugins:${GAZEBO_PLUGIN_PATH}
-gazebo --verbose ${PWD}/worlds/attitude-iris.world
+gazebo --verbose ${gazebo_assets}/worlds/betaloop_iris_arducopter_demo.world
 
